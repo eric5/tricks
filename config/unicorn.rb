e2a4d772b8b8770a9ac5c02c2d0d8d2e3e687d93
Eric Wu <ericwu1989@gmail.com>
2014-02-09 15:36:57 +0800

    add unicorn config file

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/opt/tricks"
+pid "/opt/tricks/tmp/pids/unicorn.pid"
+stderr_path "/opt/tricks/log/unicorn.log"
+stdout_path "/opt/tricks/log/unicorn.log"
+
+listen "/tmp/unicorn.shop.sock"
+worker_processes 2
+timeout 30
