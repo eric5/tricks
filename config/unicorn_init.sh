b8d855c6aca6ae7ec389338f8c0a2a246e6d8af9
Eric Wu <ericwu1989@gmail.com>
2014-02-23 17:20:54 +0800

    use mina deploy root

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/opt/tricks
+APP_ROOT=/opt/www/tricks.thetoughway.com/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=root
