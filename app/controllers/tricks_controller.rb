855c2db16c3fa884f9ad7defeab4631a89b55bc2
Eric Wu <ericwu1989@gmail.com>
2014-03-29 13:47:19 +0800

    create tricks - create tricks page - new and create tricks methods

---
+++ app/controllers/tricks_controller.rb
@@ -0,0 +1,27 @@
+class TricksController < ApplicationController
+
+  def new
+    @trick = Trick.new
+  end
+
+  def create
+    @trick = Trick.new(trick_params)
+    @trick.user_id = session[:user_id]
+    if @trick.save
+      flash[:success] = "You have created a great trick."
+      redirect_to root_path
+    else
+      redirect_to :new
+    end
+  end
+
+  def show
+    @tricks = Trick.first(10)
+  end
+
+  private
+
+  def trick_params
+    params.require(:trick).permit(:title, :content)
+  end
+end
