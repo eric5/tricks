8063806208382e00f7f4862f744cbb73605928bc
Eric Wu <ericwu1989@gmail.com>
2014-03-29 13:45:13 +0800

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-
   validates :email, :password, :name, presence: true
   validates :email, uniqueness: true
+  has_many :tricks, dependent: :destroy
 
   has_secure_password
 end
